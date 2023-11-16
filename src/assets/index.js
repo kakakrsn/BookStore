d43f87cb7ef390faa7f97ec32ac1218f6075e4a0
Kaka Karsana Noerhafidan <86177774+kakakrsn@users.noreply.github.com>
2023-11-17 06:27:35 +0700

    add new asset

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,20 @@
 import IconPlus from './icons/icon_plus.svg'
+import IconBack from './icons/icon-back.svg'
+import IconClose from './icons/IconClose.svg'
+import IconDeleteModal from './icons/IconDeleteModal.svg'
+import IconSucces from './icons/IconSucces.svg'
+
+// image
+import Profile from './img/Profile.png'
+
 
 export{
-    IconPlus,
+    IconPlus, 
+    IconBack,
+    IconClose,
+    IconDeleteModal,
+    IconSucces,
+
+    // image
+    Profile,
 }
